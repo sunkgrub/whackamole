806e66c2d2d47c64a1c9ec59952409640f36946f
sunkgrub <sunkgrub@gmail.com>
2024-10-18 16:31:36 -0400

    Replaced all LED functionality with servo functionality

--- WhackAMole_Code/WhackAMole_Code.ino
+++ WhackAMole_Code/WhackAMole_Code.ino
@@ -4,7 +4,14 @@
 // Edited by: Koen Gieskes
 // Edited further by: Aiden Rivera
 
+#include <Servo.h>
 using namespace std;
+
+// Declare individual servos
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
 
 // molePairs is a 2d array that sorts the outputs and inputs of the game
 // The format is {outputPin,inputPin}
@@ -14,6 +21,8 @@ const int molePairs[4][2] = {
   {4, 7},
   {5, 6}
 };
+
+Servo* servos[4] = {&servo1,&servo2,&servo3,&servo4};
 
 // defining the piezo buzzer
 const int Piezo = 10;
@@ -26,26 +35,26 @@ int elapsed = 0;
 int score = 0;
 
 // Global light states
-bool lightsOn[4] = {1, 1, 1, 1};
-bool lightsOff[4] = {0, 0, 0, 0};
+bool molesUp[4] = {1, 1, 1, 1};
+bool molesDown[4] = {0, 0, 0, 0};
 
 // sets up the variables as inputs or outputs
-void display_lights(bool lights[4]) {
+void displayMoles(bool moles[4]) {
   for (int i = 0; i < 4; i++) {
-    digitalWrite(molePairs[i][0], lights[i]);
+    digitalWrite(molePairs[i][0], moles[i]);
   }
 }
 
 void checkMole(int mole) {
-  if (digitalRead(molePairs[mole][0]) == HIGH) {
-    // reads if the button corresponding to the red LED is ON
-    // if redButton is ON proceed into if statement
+  if (servos[mole]->read() >= 179) {
+    // reads if the button corresponding to the mole is up
+    // if correct button is ON proceed into if statement
     if (digitalRead(molePairs[mole][1]) == HIGH) {
       // shows that a point was gotten
-      display_lights(lightsOn);
+      displayMoles(molesUp);
       delay(250); // in milliseconds
       
-      display_lights(lightsOff);
+      displayMoles(molesDown);
       delay(150); // in milliseconds
       
       // turns the piezo button ON
@@ -59,20 +68,23 @@ void checkMole(int mole) {
       // adds 1 point to your score
       score++;
 
-      // turns OFF the LED light
-      digitalWrite(molePairs[mole][0], LOW);
+      // turns OFF the LED moles
+      servos[mole]->write(0);
     }
   }
 }
 
 void setup() {
-  // initialize the LED lights as OUTPUTS
+  // initialize the servos
   // initialize the corresponding buttons as INPUTS
   //(can also initialize as INPUT_PULLUP 
   // which sets the buttons to HIGH when unpressed)
   for (int i = 0; i < 4; i++) {
-    pinMode(molePairs[i][0], OUTPUT);
     pinMode(molePairs[i][1], INPUT);
+  }
+
+  for (int i = 1; i < 4; i++) {
+    servos[i]->attach(molePairs[i][0]);
   }
   
   // initialize the random variable pick
@@ -87,51 +99,51 @@ void loop() {
   // starts the game with a score of 0
   // will go through this loop until score hits 4
   while (score < 4) {
-    // define randomColor as integer variable 
-    int randomColor;
+    // define randomMole as integer variable 
+    int randomMole;
     
-    // generate random # from 0 to 4 that matches LED Pins
-    randomColor = random(0, 4);
+    // generate random # from 0 to 4 that matches servo Pins
+    randomMole = random(0, 4);
     
-    digitalWrite(molePairs[randomColor][0], HIGH);
+    servos[randomMole]->write(180);
     
     starttime = millis();
     elapsed = 0;
 
     while (elapsed <= 1000) {
       for (int i = 0; i < 4; i++) {
-        if (digitalRead(molePairs[i][0]) == HIGH) {
+        if (servos[i]->read() >= 179) {
           checkMole(i);
         }
       }
       elapsed = millis() - starttime;
       delay(150);
     }
-    display_lights(lightsOff); // Turn off all moles
+    displayMoles(molesDown); // Turn off all moles
   }
 
   score = 0;
   // Shows game is over
-  display_lights(lightsOn); // Turn on all lights to indicate game over
+  displayMoles(molesUp); // Turn on all moles to indicate game over
   delay(500);
 
-  display_lights(lightsOff); // Turn off lights
+  displayMoles(molesDown); // Turn off moles
   delay(400);
 
-  display_lights(lightsOn); // Turn on lights
+  displayMoles(molesUp); // Turn on moles
   delay(300);
 
-  display_lights(lightsOff); // Turn off lights
+  displayMoles(molesDown); // Turn off moles
   delay(200);
 
-  display_lights(lightsOn); // Turn on lights
+  displayMoles(molesUp); // Turn on moles
   delay(200);
 
-  display_lights(lightsOff); // Turn off lights
+  displayMoles(molesDown); // Turn off moles
   delay(100);
 
-  display_lights(lightsOn); // Turn on lights
+  displayMoles(molesUp); // Turn on moles
   delay(500);
 
-  display_lights(lightsOff); // Final turn off
+  displayMoles(molesDown); // Final turn off
 }

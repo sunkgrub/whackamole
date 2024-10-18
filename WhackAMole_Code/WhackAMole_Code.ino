e022d54f3fb5c932a645480132b7382d0b923f4d
sunkgrub <sunkgrub@gmail.com>
2024-10-18 00:42:42 -0400

    Fully implemeneted array functionality and made the program completely generalized

--- WhackAMole_Code/WhackAMole_Code.ino
+++ WhackAMole_Code/WhackAMole_Code.ino
@@ -4,200 +4,134 @@
 // Edited by: Koen Gieskes
 // Edited further by: Aiden Rivera
 
-//define variables used in code
-//variables associated with digital pin number
-//defining LED lights
-
-#include <map>
-#include <iostream>
-
 using namespace std;
 
-//molePairs is a 2d array that sorts the outputs and inputs of the game
-//The format is {outputPin,inputPin}
-int molePairs[4][2] = {
-  {2,9},
-  {3,8},
-  {4,7},
-  {5,6}
-}
+// molePairs is a 2d array that sorts the outputs and inputs of the game
+// The format is {outputPin,inputPin}
+const int molePairs[4][2] = {
+  {2, 9},
+  {3, 8},
+  {4, 7},
+  {5, 6}
+};
 
-//defining the piezo buzzer
+// defining the piezo buzzer
 const int Piezo = 10;
 
-//defining time variables
+// defining time variables
 int starttime = 0;
 int elapsed = 0;
 
-//defining the score
-int score;
+// defining the score
+int score = 0;
 
-//sets up the the variables as inputs or outputs
+// Global light states
+bool lightsOn[4] = {1, 1, 1, 1};
+bool lightsOff[4] = {0, 0, 0, 0};
 
+// sets up the variables as inputs or outputs
 void display_lights(bool lights[4]) {
-  for (int i=0; i < 4; i++) {
-    digitalWrite(molePairs[i][0],lights[i]);
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(molePairs[i][0], lights[i]);
   }
-
-
 }
 
 void checkMole(int mole) {
-  if(digitalRead(molePairs[mole][0])==HIGH)
-      {
-        //reads if the button corrosponding to the redLED is ON
-        //if redButton is ON proceed into if statement
-        if(digitalRead(molePairs[mole][1])==HIGH)
-        {
-          //shows that a point was gotten
-          display_lights(1,1,1,1);
-          delay(250);//in milliseconds
-          
-          display_lights(0,0,0,0);
-          delay(250);//in milliseconds
-          
-          //turns the piezo button ON
-          tone(Piezo,250);
-          
-          delay(200);//in milliseconds
-          
-          //turns the piezo button OFF
-          noTone(Piezo);
-          
-          //adds 1 point to your score
-          score++;
+  if (digitalRead(molePairs[mole][0]) == HIGH) {
+    // reads if the button corresponding to the red LED is ON
+    // if redButton is ON proceed into if statement
+    if (digitalRead(molePairs[mole][1]) == HIGH) {
+      // shows that a point was gotten
+      display_lights(lightsOn);
+      delay(250); // in milliseconds
+      
+      display_lights(lightsOff);
+      delay(150); // in milliseconds
+      
+      // turns the piezo button ON
+      tone(Piezo, 250);
+      
+      delay(200); // in milliseconds
+      
+      // turns the piezo button OFF
+      noTone(Piezo);
+      
+      // adds 1 point to your score
+      score++;
 
-        //turns OFF the LED light
-        digitalWrite(redLED, LOW);
-        }
-      }
+      // turns OFF the LED light
+      digitalWrite(molePairs[mole][0], LOW);
+    }
+  }
 }
 
-void setup()
-{
-  //initialize the LED lights as OUTPUTS
-  pinMode(redLED, OUTPUT);
-  pinMode(blueLED, OUTPUT);
-  pinMode(yellowLED, OUTPUT);
-  pinMode(greenLED, OUTPUT);
-
-  //initialize the corrresponding buttons as INPUTS
+void setup() {
+  // initialize the LED lights as OUTPUTS
+  // initialize the corresponding buttons as INPUTS
   //(can also initialize as INPUT_PULLUP 
-    //which sets the buttons to HIGH when unpressed)
-  pinMode(redButton, INPUT);
-  pinMode(blueButton, INPUT);
-  pinMode(yellowButton, INPUT);
-  pinMode(greenButton, INPUT);
+  // which sets the buttons to HIGH when unpressed)
+  for (int i = 0; i < 4; i++) {
+    pinMode(molePairs[i][0], OUTPUT);
+    pinMode(molePairs[i][1], INPUT);
+  }
   
-  //initialize the random variable pick
+  // initialize the random variable pick
   randomSeed(analogRead(0));
   
-  //initialize the piezo buzzer
+  // initialize the piezo buzzer
   pinMode(Piezo, OUTPUT);
-
-  //Creating map of buttons and leds
-  map<int,int> buttonMoleMap = {
-    {redLED,redButton},
-    {blueLED, blueButton},
-    {yellowLED, yellowButton},
-    {greenLED, greenButton}
-  };
-
 }
 
-//main function loop
-void loop()
-{
-  //starts the game with a score of 0
-  //will go through this loop until score hits 3
-  while(score < 4)
-  {
-    //define randomColor as integer variable 
+// main function loop
+void loop() {
+  // starts the game with a score of 0
+  // will go through this loop until score hits 4
+  while (score < 4) {
+    // define randomColor as integer variable 
     int randomColor;
     
-    //generate random # from 2 to 5 that matches led Pins
-    //2 = red, 3 = blue, 4 = green, 5 = yellow
-    randomColor = random(2,6);
+    // generate random # from 0 to 4 that matches LED Pins
+    randomColor = random(0, 4);
     
-    if(randomColor==2)
-    {
-      //digitalWrite function turns the LED ON or OFF
-      //HIGH means turning the LED ON
-      digitalWrite(redLED, HIGH);
-    }
-    
-    if(randomColor==3)
-    {
-      //digitalWrite function turns the LED ON or OFF
-      //HIGH means turning the LED ON
-      digitalWrite(blueLED, HIGH);
-    }
-    
-    if(randomColor==4)
-    {
-      //digitalWrite function turns the LED ON or OFF
-      //HIGH means turning the LED ON
-      digitalWrite(greenLED, HIGH);
-    }
-      
-    if(randomColor==5)
-    {
-      //digitalWrite function turns the LED ON or OFF
-      //HIGH means turning the LED ON
-      digitalWrite(yellowLED, HIGH);
-    }
+    digitalWrite(molePairs[randomColor][0], HIGH);
     
     starttime = millis();
     elapsed = 0;
 
-    //digitalRead function reads whether I/O are HIGH or LOW
-    //[||] denotes "or"
-    while((digitalRead(redLED)==HIGH||digitalRead(blueLED)==HIGH||digitalRead(yellowLED)==HIGH||digitalRead(greenLED)==HIGH)&&(elapsed<=1000))
-    {
-      //reads if the redLED is ON (HIGH)
-      //if redLED is ON continue into if statement if not move on
-      checkMole(redLED);
-      
-      //reads if the blueLED is ON (HIGH)
-      //if blueLED is ON continue into if statement if not move on
-      checkMole(blueLED);
-      
-      //reads if the yellowLED is ON (HIGH)
-      //if yellowLED is ON continue into if statement if not move on
-      checkMole(yellowLED);
-      
-      //reads if the greenLED is ON (HIGH)
-      //if greenLED is ON continue into if statement if not move on
-      checkMole(greenLED);
-      
-      elapsed = millis()-starttime;
-      delay(150);//in milliseconds
+    while (elapsed <= 1000) {
+      for (int i = 0; i < 4; i++) {
+        if (digitalRead(molePairs[i][0]) == HIGH) {
+          checkMole(i);
+        }
+      }
+      elapsed = millis() - starttime;
+      delay(150);
     }
-    display_lights(0,0,0,0);
-
+    display_lights(lightsOff); // Turn off all moles
   }
+
   score = 0;
-  //Shows game is over
-  display_lights(1,1,1,1);
+  // Shows game is over
+  display_lights(lightsOn); // Turn on all lights to indicate game over
   delay(500);
 
-  display_lights(0,0,0,0);
+  display_lights(lightsOff); // Turn off lights
   delay(400);
 
-  display_lights(1,1,1,1);
+  display_lights(lightsOn); // Turn on lights
   delay(300);
 
-  display_lights(0,0,0,0);
+  display_lights(lightsOff); // Turn off lights
   delay(200);
 
-  display_lights(1,1,1,1);
+  display_lights(lightsOn); // Turn on lights
   delay(200);
 
-  display_lights(0,0,0,0);
+  display_lights(lightsOff); // Turn off lights
   delay(100);
 
-  display_lights(1,1,1,1);
+  display_lights(lightsOn); // Turn on lights
   delay(500);
 
-  display_lights(0,0,0,0);
+  display_lights(lightsOff); // Final turn off
 }

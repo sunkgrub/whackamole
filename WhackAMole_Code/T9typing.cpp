6dd49ba9b7f4b0588b0642e5a7add0a9953eddd0
sunkgrub <sunkgrub@gmail.com>
2024-10-30 17:55:49 -0400

    s

--- WhackAMole_Code/T9typing.cpp
+++ WhackAMole_Code/T9typing.cpp
@@ -1,28 +1,3 @@
-#include <stdio.h>
-
-void typing();
-char currentKey;
-
-const char* numDict[] = {
-    "abc", 
-    "def", 
-    "ghi",
-    "pqrs",
-    "tuv", 
-    "wxyz"
-};
-
-int main () {
-    printf("typing active\n");
-    typing();
+int main() {
     return 0;
-}
-
-void typing() {
-    while (true) {
-        scanf("%c",&currentKey);
-        if (currentKey == '*') {
-            break;
-        }
-    }
 }

33ed20e9f899080fded960c6a2846509ed43a0a7
sunkgrub <sunkgrub@gmail.com>
2024-10-30 17:32:36 -0400

    Added typing function

--- WhackAMole_Code/T9typing.cpp
+++ WhackAMole_Code/T9typing.cpp
@@ -1,12 +1,28 @@
 #include <stdio.h>
 
+void typing();
+char currentKey;
+
+const char* numDict[] = {
+    "abc", 
+    "def", 
+    "ghi",
+    "pqrs",
+    "tuv", 
+    "wxyz"
+};
+
 int main () {
-    printf("Hello world! 2");
+    printf("typing active\n");
+    typing();
     return 0;
 }
 
 void typing() {
     while (true) {
-        
+        scanf("%c",&currentKey);
+        if (currentKey == '*') {
+            break;
+        }
     }
 }
